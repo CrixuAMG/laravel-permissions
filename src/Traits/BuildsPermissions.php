e83650e691eb7ee34d14170d12a0ea69c927ff24
Christian <christian@freshapple.nl>
2018-07-11 12:55:55 +0200

    Added new BuildsPermissions trait

---
+++ src/Traits/BuildsPermissions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CrixuAMG\Permissions\Traits;
+
+trait BuildsPermissions
+{
+    public function permission(string $permission, string $minimumRole = null, string ...$roleExceptions)
+    {
+        // TODO: build this
+    }
+}
--- src/config/permissions.php
+++ src/config/permissions.php
@@ -17,5 +17,12 @@ return [
     'models' => [
         'permissions' => Permission,
         'roles' => Role,
-    ]
+    ],
+
+    'roles' => [
+        // Register any roles here, recommended is to use class constants to have a single declaration of role names
+        'admin' => trans('roles.admin'),
+        'moderator' => trans('roles.moderator'),
+        'member' => trans('roles.member'),
+    ],
 ];
